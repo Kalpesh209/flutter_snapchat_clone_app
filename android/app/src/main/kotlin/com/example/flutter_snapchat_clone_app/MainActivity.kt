296d5d7abd59b00721ca7f141f9497e9713d59a6
Kalpesh Khandla <khandlakalpesh92@gmail.com>
2021-05-14 22:31:15 -0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/flutter_snapchat_clone_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_snapchat_clone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_snapchat_clone_app/screens/home_page_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Snapchat Clone App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePageScreen(),
+    );
+  }
+}
